2bba7a595efc6c8843b0367206140064a03dc033
Michael Fellinger <m.fellinger@gmail.com>
2013-11-19 18:01:22 +0100

    Further cleanup, move a bunch of comments to NOTES.md

--- parser.go
+++ parser.go
@@ -56,13 +56,13 @@ func NewParser(data []byte) *Parser {
 		ClassInfosIdMapping:   map[string]int{},
 		ClassInfosNameMapping: map[int]string{},
 		CombatLog:             []*CombatLogEntry{},
+		ConVars:               map[string]interface{}{},
 		GameEventMap:          map[int32]*dota.CSVCMsg_GameEventListDescriptorT{},
 		Mapping:               map[int][]*send_tables.SendProp{},
 		Multiples:             map[int]map[string]int{},
 		PlayerResourceIndex:   -1,
 		RawClicks:             []*RawClick{},
 		VoiceData:             map[int][]byte{},
-		ConVars:               map[string]interface{}{},
 	}
 }
 
@@ -94,6 +94,9 @@ func (p *Parser) Parse() {
 			buffer = append(buffer, item)
 		}
 	})
+
+	// and process all remaining in the last tick
+	p.processTick(buffer)
 }
 
 func (p *Parser) processTick(items []*parser.ParserBaseItem) {
@@ -124,130 +127,59 @@ func (p *Parser) processTick(items []*parser.ParserBaseItem) {
 			*dota.CDemoStringTables, *dota.CDemoFileHeader, *dota.CSVCMsg_ServerInfo,
 			*dota.CDemoClassInfo, *dota.CSVCMsg_SendTable,
 			*dota.CSVCMsg_GameEventList:
+			// those have been handled above, please keep in sync.
+		case *dota.CDemoSyncTick,
+			*dota.CDOTAUserMsg_CreateLinearProjectile,
+			*dota.CDOTAUserMsg_DestroyLinearProjectile,
+			*dota.CDOTAUserMsg_DodgeTrackingProjectiles,
+			*dota.CDOTAUserMsg_GlobalLightColor,
+			*dota.CDOTAUserMsg_GlobalLightDirection,
+			*dota.CDOTAUserMsg_HalloweenDrops,
+			*dota.CDOTAUserMsg_HudError,
+			*dota.CDOTAUserMsg_LocationPing,
+			*dota.CDOTAUserMsg_MapLine,
+			*dota.CDOTAUserMsg_MinimapEvent,
+			*dota.CDOTAUserMsg_NevermoreRequiem,
+			*dota.CDOTAUserMsg_ParticleManager,
+			*dota.CDOTAUserMsg_SendRoshanPopup,
+			*dota.CDOTAUserMsg_SendStatPopup,
+			*dota.CDOTAUserMsg_SharedCooldown,
+			*dota.CDOTAUserMsg_UnitEvent,
+			*dota.CDOTAUserMsg_WorldLine,
+			*dota.CNETMsg_Tick,
+			*dota.CSVCMsg_ClassInfo,
+			*dota.CSVCMsg_SetView,
+			*dota.CSVCMsg_Sounds,
+			*dota.CSVCMsg_TempEntities,
+			*dota.CUserMsg_SendAudio,
+			*dota.CUserMsg_TextMsg,
+			*dota.CUserMsg_VoiceMask:
+			// see NOTES for why we ignore them.
+		case *dota.CDemoFileInfo:
+			p.onFileInfo(obj)
+		case *dota.CDOTAUserMsg_ChatEvent:
+			p.onChatEvent(item.Tick, obj)
+		case *dota.CDOTAUserMsg_OverheadEvent:
+			p.onOverheadEvent(item.Tick, obj)
+		case *dota.CDOTAUserMsg_SpectatorPlayerClick:
+			p.onSpectatorPlayerClick(item.Tick, obj)
 		case *dota.CNETMsg_SetConVar:
-			// (convars:<cvars:<name:"sv_consistency" value:"0" > cvars:<name:"sv_skyname" value:"sky_dotasky_01" > cvars:<name:"tv_transmitall" value:"1" > cvars:<name:"steamworks_sessionid_server" value:"21537759004" > cvars:<name:"think_limit" value:"0" > cvars:<name:"tv_transmitall" value:"1" > > )
 			p.onSetConVar(obj)
 		case *dota.CNETMsg_SignonState:
-			// we see signon_state:3|4|5 before the SyncTick
-			// (signon_state:3 spawn_count:2 num_server_players:0 )
-		case *dota.CDemoSyncTick:
-			// that's where the game timer starts?
-		case *dota.CNETMsg_Tick:
-			// no idea what this is good for, but there seems to be only one.
-			// (tick:109 host_frametime:0 host_frametime_std_deviation:0 )
-		case *dota.CSVCMsg_SetView:
-			// unfortunately no clue wtf this means.
-			// (entity_index:1 )
-			// (entity_index:2 )
+			p.onSignonState(obj)
 		case *dota.CSVCMsg_GameEvent:
 			p.onGameEvent(item.Tick, obj)
+		case *dota.CSVCMsg_VoiceInit:
+			p.VoiceInit = obj
+		case *dota.CUserMsg_SayText2:
+			p.onSayText2(obj)
 		case *dota.CSVCMsg_PacketEntities:
-			// FIXME: if we ever want to be able to parse at a specific time, we need
-			//        to handle all.
+			// to skip to a specific time, we have to handle more.
 			if item.From == dota.EDemoCommands_DEM_Packet {
 				p.ParsePacket(item.Tick, obj)
 			}
-		case *dota.CSVCMsg_ClassInfo:
-			// (create_on_client:true )
-		case *dota.CSVCMsg_VoiceInit:
-			// Defines what codec is used for the speech codec of casters.
-			// (quality:5 codec:"vaudio_celt" version:3 )
-			p.VoiceInit = obj
-		case *dota.CUserMsg_TextMsg:
-			// Need more samples, but seems not used a lot.
-			// (dest:2 param:"Randoming npc_dota_hero_broodmother!\n")
-		case *dota.CUserMsg_SayText2:
-			// what everyone is saying.
-			p.AllChat = append(p.AllChat, obj)
-		case *dota.CDOTAUserMsg_SpectatorPlayerClick:
-			// (entindex:1 order_type:1 target_index:0 )
-			// Couple with mouse position of player entity and you got yourself a real click.
-			// If i can decipher the target_index it could track spells without damage or (de)buff.
-			// Finding out the mouse location in map coordinates is way harder due to
-			// the amount of possible world angles and rotations.
-			p.RawClicks = append(p.RawClicks, &RawClick{
-				Tick:   item.Tick,
-				Entity: int(obj.GetEntindex()),
-				Type:   int(obj.GetOrderType()),
-				Target: int(obj.GetTargetIndex()),
-			})
-		case *dota.CDOTAUserMsg_ChatEvent:
-			p.onChatEvent(item.Tick, obj)
-		case *dota.CDOTAUserMsg_ParticleManager:
-			// So much value here, but so hard to use.
-		case *dota.CSVCMsg_TempEntities:
-			// Temporary entities are used by the server to create short-lived or
-			// one-off effects on clients. They are different to standard entities in
-			// that they are 'fire and forget'; once one has been created, the server
-			// has nothing more to do with it. TEs have no edict or entity index, and
-			// do not count toward the entity limit.
-			//
-			// TEs are unreliable and get dropped if too many are created at once.
-			// The maximum per update is 32 in multiplayer and 255 in single player.
-		case *dota.CDOTAUserMsg_CreateLinearProjectile:
-			// (origin:<x:-849.5365 y:-2277 z:127.99994 > velocity:<x:687.15155 y:-1458.2067 > entindex:743 particle_index:1998 handle:70 )
-		case *dota.CDOTAUserMsg_DestroyLinearProjectile:
-			// (handle:70 )
-		case *dota.CDOTAUserMsg_DodgeTrackingProjectiles:
-		// (entindex:743 )
-		case *dota.CSVCMsg_Sounds:
-			// this may be useful in tracing the location of some skills, but is
-			// neither reliable nor desireable, so ignored for now.
-			// (reliable_sound:false sounds:<origin_x:700 origin_y:-642 origin_z:36 entity_index:1186 pitch:95 flags:1024 sound_num_handle:928353761 random_seed:127 sound_level:72 > sounds:<origin_x:657 origin_y:-675 entity_index:1193 pitch:98 sound_num_handle:1453582671 random_seed:33 > sounds:<origin_x:692 origin_y:-648 entity_index:1353 pitch:102 sound_num_handle:635720003 random_seed:60 > )
-		case *dota.CDOTAUserMsg_UnitEvent:
-			// we could track things like how many times a player cancels certain
-			// animations or is interrupted while trying to cast.
-			// Overall doesn't seem especially worthwhile.
-
-			switch obj.GetMsgType() {
-			case dota.EDotaEntityMessages_DOTA_UNIT_ADD_GESTURE:
-				// unit starts animation
-			case dota.EDotaEntityMessages_DOTA_UNIT_FADE_GESTURE:
-				// might be animation backswing
-			case dota.EDotaEntityMessages_DOTA_UNIT_REMOVE_GESTURE:
-			case dota.EDotaEntityMessages_DOTA_UNIT_REMOVE_ALL_GESTURES:
-				// maybe on death?
-			case dota.EDotaEntityMessages_DOTA_UNIT_SPEECH:
-				// heroes say stuff, shows animation in the portrait.
-			case dota.EDotaEntityMessages_DOTA_UNIT_SPEECH_CLIENTSIDE_RULES:
-				// the announcer says stuff
-			default:
-				spew.Dump(obj)
-			}
-		case *dota.CDOTAUserMsg_MinimapEvent:
-			// various kinds of pings, i think.
-			// (event_type:4 entity_handle:1652244 x:1520 y:-416 duration:4)
-			// (event_type:32 entity_handle:1039755 x:-1864 y:-1740 duration:3 )
-			// (event_type:32 entity_handle:35559 x:-1283 y:1418 duration:5 )
-			// (event_type:4 entity_handle:1095158 x:-878 y:-694 duration:4 )
-			// (event_type:64 entity_handle:1039755 x:-1410 y:1329 duration:4 )
-		case *dota.CDOTAUserMsg_GlobalLightColor:
-			// extremely rare, maybe only on very specific ability?
-			// (color:32214126 duration:0.145 )
-		case *dota.CDOTAUserMsg_GlobalLightDirection:
-			// extremely rare, maybe only on very specific ability?
-			// (direction:<x:-0.53466 y:0.13 z:-0.9 > duration:0.145 )
 		case *dota.CDemoStop:
 			// the demo ends here.
-		case *dota.CDemoFileInfo:
-			// this shows up as a summary, unfortunately only at the end of the file.
-			// (playback_time:346.93335 playback_ticks:10408 playback_frames:4886 )
-			p.FileInfo = obj
-		case *dota.CUserMsg_SendAudio:
-			// global sounds?
-			// (stop:false name:"GameStart.DireAncient")
-			// (stop:false name:"GameStart.RadiantAncient")
-			// (stop:false name:"General.Night")
-		case *dota.CDOTAUserMsg_OverheadEvent:
-			p.onOverheadEvent(item.Tick, obj)
-		case *dota.CDOTAUserMsg_SharedCooldown:
-			// this was introduced around the first blood patch, helps to track
-			// shared item cooldowns for some edge cases. Still have to figure out a
-			// usecase for us.
-			// (entindex:1283 cooldown:4 name_index:2 )
-			// (entindex:1409 cooldown:17 name_index:14 )
-			// (entindex:1409 cooldown:0.5 name_index:19 )
-			// (entindex:1283 cooldown:5 name_index:3 )
 		case *dota.CSVCMsg_VoiceData:
 			/*
 				client := int(obj.GetClient())
@@ -258,39 +190,31 @@ func (p *Parser) processTick(items []*parser.ParserBaseItem) {
 					p.VoiceData[client] = obj.GetVoiceData()
 				}
 			*/
-		case *dota.CDOTAUserMsg_HudError:
-			// looks like some kind of notification for upcoming StringTableUpdate
-			// (order_id:19 )
-			// (*dota.CSVCMsg_UpdateStringTable)(table_id:19 num_changed_entries:1 string_data:"\360\301\005\000A\000\302\261\320\006C\000$\032\n" )
-		case *dota.CDOTAUserMsg_MapLine:
-			// I guess those are lines drawn?
-			// (player_id:2 mapline:<x:-7813 y:-5988 initial:false > )
-		case *dota.CDOTAUserMsg_WorldLine:
-			// now some spectator drawing on the world?
-			// (player_id:22 worldline:<x:-1820 y:-1819 z:127 initial:true > )
-		case *dota.CUserMsg_VoiceMask:
-			// this might be used for mute status of players, haven't found a use for it.
-		case *dota.CDOTAUserMsg_SendStatPopup:
-			// (player_id:16 statpopup:<style:k_EDOTA_SPT_Textline stat_strings:"I spoke to Bruno in the break, he called Na`Vi's strat last game \"Electric Bear\"" > )
-			// (player_id:16 statpopup:<style:k_EDOTA_SPT_Basic stat_strings:"Chat Poll Results" stat_strings:"62% of viewers expected TobiWan to miss first blood.\rThey were wrong." stat_images:1001 > )
-		case *dota.CDOTAUserMsg_SendRoshanPopup:
-			// (reclaimed:true gametime:2075 )
-			// (reclaimed:false gametime:2314 )
-		case *dota.CDOTAUserMsg_NevermoreRequiem:
-			// (entity_handle:605564 lines:18 origin:<x:40.487564 y:2291.1592 z:256 > )
-		case *dota.CDOTAUserMsg_LocationPing:
-		// pings, might be useful later
-		// (player_id:1 location_ping:<x:-3513 y:-5111 target:1063 direct_ping:true type:0 > )
 		default:
 			spew.Dump(obj)
 		}
 	}
 }
 
+func (p *Parser) onSpectatorPlayerClick(tick int, obj *dota.CDOTAUserMsg_SpectatorPlayerClick) {
+	p.RawClicks = append(p.RawClicks, &RawClick{
+		Tick:   tick,
+		Entity: int(obj.GetEntindex()),
+		Type:   int(obj.GetOrderType()),
+		Target: int(obj.GetTargetIndex()),
+	})
+}
+
 func (p *Parser) onSetConVar(obj *dota.CNETMsg_SetConVar) {
 	for _, cvar := range obj.GetConvars().GetCvars() {
 		p.ConVars[cvar.GetName()] = cvar.GetValue()
 	}
+}
+
+func (p *Parser) onSignonState(obj *dota.CNETMsg_SignonState) {}
+
+func (p *Parser) onSayText2(obj *dota.CUserMsg_SayText2) {
+	p.AllChat = append(p.AllChat, obj)
 }
 
 // http://www.cyborgmatt.com/2013/01/dota-2-replay-parser-bruno/#cdotausermsg-overheadevent
@@ -308,6 +232,12 @@ func (p *Parser) onSetConVar(obj *dota.CNETMsg_SetConVar) {
 // (message_type:OVERHEAD_ALERT_GOLD value:121 target_player_entindex:1 target_entindex:1052 )
 // (message_type:OVERHEAD_ALERT_MISS value:0 target_entindex:1039 )
 func (p *Parser) onOverheadEvent(tick int, obj *dota.CDOTAUserMsg_OverheadEvent) {
+}
+
+// this shows up as a summary, unfortunately only at the end of the file.
+// (playback_time:346.93335 playback_ticks:10408 playback_frames:4886 )
+func (p *Parser) onFileInfo(obj *dota.CDemoFileInfo) {
+	p.FileInfo = obj
 }
 
 // http://www.cyborgmatt.com/2013/01/dota-2-replay-parser-bruno/#chatevents
@@ -472,12 +402,16 @@ func (p *Parser) ParsePacket(tick int, pe *dota.CSVCMsg_PacketEntities) {
 		currentIndex = br.ReadNextEntityIndex(currentIndex)
 		uType := packet_entities.ReadUpdateType(br)
 		switch uType {
-		case packet_entities.Preserve:
-			p.EntityPreserve(br, currentIndex, tick)
 		case packet_entities.Create:
 			p.EntityCreate(br, currentIndex, tick)
+		case packet_entities.Preserve:
+			p.EntityPreserve(br, currentIndex, tick)
 		case packet_entities.Delete:
 			p.EntityDelete(br, currentIndex, tick)
+		case packet_entities.Leave:
+			// TODO: figure out what this is good for.
+		default:
+			panic("new meta!")
 		}
 	}
 }
@@ -546,26 +480,6 @@ func (p *Parser) EntityDelete(br *utils.BitReader, currentIndex, tick int) {
 	if p.EntityDeleted != nil {
 		p.EntityDeleted(pe)
 	}
-}
-
-func (p *Parser) PicksBans() (picksbans []*PickBan, isCaptainsMode bool) {
-	gameInfo := p.FileInfo.GetGameInfo()
-	game := gameInfo.GetDota()
-
-	if game.GetGameMode() != 2 {
-		return nil, false
-	}
-
-	pbs := make([]*PickBan, 0, 10)
-	for _, pb := range game.GetPicksBans() {
-		pbs = append(pbs, &PickBan{
-			IsPick: pb.GetIsPick(),
-			Team:   int(pb.GetTeam() - 2),
-			HeroId: int(pb.GetHeroId()),
-		})
-	}
-
-	return pbs, true
 }
 
 // logs chronologically close to this event (usually within 1-2 ticks).
